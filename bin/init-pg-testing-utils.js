3e59eec56caedc5365f3e7071ca76e18d0767e58
Joe Bailey <bailejl@yahoo.com>
2015-05-03 21:51:16 -0700

    Fixed some bugs with the download of wiremock jar

--- bin/init-pg-testing-utils.js
+++ bin/init-pg-testing-utils.js
@@ -4,12 +4,16 @@ var http = require('http'),
     fs = require('fs');
 
 var platform = process.platform,
+    downloadDir = __dirname + '/downloads/',
     wiremockJarPath = 'http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock/1.55/',
     wiremockJarFileName = 'wiremock-1.55-standalone.jar';
 
 console.log('Downloading: ' + wiremockJarPath + wiremockJarFileName);
 
 var request = http.get(wiremockJarPath + wiremockJarFileName, function(response) {
-    var file = fs.createWriteStream(__dirname + wiremockJarFileName);
+    if (!fs.existsSync(downloadDir)){
+        fs.mkdirSync(downloadDir);
+    }
+    var file = fs.createWriteStream(downloadDir + wiremockJarFileName);
     response.pipe(file);
 });
